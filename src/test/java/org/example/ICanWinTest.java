a0b40e57ec4ee832f5abfb5775ae2583fca6463f
ten <tenzinseventy3@gmail.com>
2023-05-05 11:54:41 +0530

    1st

---
+++ src/test/java/org/example/ICanWinTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ICanWinTest {
+    WebDriver driver;
+    @BeforeClass
+    void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://pastebin.com/");
+        driver.manage().window().maximize();
+    }
+    @Test ( priority = 1)
+    void NewPaste() throws InterruptedException {
+
+        ICanWin helloWeb= new ICanWin(driver);
+        helloWeb.Code("Hello from WebDriver");
+        helloWeb.pasteExpiration();
+        helloWeb.PasteName("helloweb");
+        helloWeb.CreatedPaste();
+        driver.quit();
+    }
+}
